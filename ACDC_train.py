d208349324b152aea0b5fc2c237b2f818c2ec1c2
suofer <61673547+suofer@users.noreply.github.com>
2022-09-27 08:56:53 +0800

    Add files via upload

---
+++ ACDC_train.py
@@ -0,0 +1,94 @@
+import argparse
+import logging
+import os
+import random
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from networks.vision_transformer import SwinUnet as ViT_seg
+from Aug_trainer import trainer_synapse
+from config import get_config
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str,
+                    default='../data/Synapse/train_npz', help='root dir for data')
+parser.add_argument('--dataset', type=str,
+                    default='ACDC', help='experiment_name')
+parser.add_argument('--list_dir', type=str,
+                    default='./lists/lists_Synapse', help='list dir')
+parser.add_argument('--num_classes', type=int,
+                    default=4, help='output channel of network')
+parser.add_argument('--max_iterations', type=int,
+                    default=30000, help='maximum epoch number to train')
+parser.add_argument('--max_epochs', type=int,
+                    default=250, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type=int,
+                    default=12, help='batch_size per gpu')
+parser.add_argument('--n_gpu', type=int, default=1, help='total gpu')
+parser.add_argument('--deterministic', type=int,  default=1,
+                    help='whether use deterministic training')
+parser.add_argument('--base_lr', type=float,  default=0.01,
+                    help='segmentation network learning rate')
+parser.add_argument('--img_size', type=int,
+                    default=224, help='input patch size of network input')
+parser.add_argument('--seed', type=int,
+                    default=1234, help='random seed')
+parser.add_argument('--n_skip', type=int,
+                    default=3, help='using number of skip-connect, default is num')
+parser.add_argument('--vit_name', type=str,
+                    default='R50-ViT-B_16', help='select one vit model')
+parser.add_argument('--vit_patches_size', type=int,
+                    default=16, help='vit_patches_size, default is 16')
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    if not args.deterministic:
+        cudnn.benchmark = True
+        cudnn.deterministic = False
+    else:
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    dataset_name = args.dataset
+    dataset_config = {
+        'Synapse': {
+            'root_path': '/public/home/jd_fly/code/project_TransUNet/data/Synapse/train_npz',
+            'list_dir': '/public/home/jd_fly/code/new_list/lists_Synapse',
+            'num_classes': 9,
+        },
+        'ACDC': {
+            'root_path': '/public/home/jd_fly/code/Datasets/ACDC/train_npz',
+            'list_dir': '/public/home/jd_fly/code/Datasets/ACDC/lists',
+            'num_classes': 4,
+        },
+    }
+    args.num_classes = dataset_config[dataset_name]['num_classes']
+    args.root_path = dataset_config[dataset_name]['root_path']
+    args.list_dir = dataset_config[dataset_name]['list_dir']
+    args.is_pretrain = True
+    args.exp = 'TU_' + dataset_name + str(args.img_size)
+    snapshot_path = "./model/{}/{}".format(args.exp, 'TU')
+    snapshot_path = snapshot_path + '_pretrain' if args.is_pretrain else snapshot_path
+    snapshot_path += '_' + args.vit_name
+    snapshot_path = snapshot_path + '_skip' + str(args.n_skip)
+    snapshot_path = snapshot_path + '_vitpatch' + str(args.vit_patches_size) if args.vit_patches_size!=16 else snapshot_path
+    snapshot_path = snapshot_path+'_'+str(args.max_iterations)[0:2]+'k' if args.max_iterations != 30000 else snapshot_path
+    snapshot_path = snapshot_path + '_epo' +str(args.max_epochs) if args.max_epochs != 30 else snapshot_path
+    snapshot_path = snapshot_path+'_bs'+str(args.batch_size)
+    snapshot_path = snapshot_path + '_lr' + str(args.base_lr) if args.base_lr != 0.01 else snapshot_path
+    snapshot_path = snapshot_path + '_'+str(args.img_size)
+    snapshot_path = snapshot_path + '_s'+str(args.seed) if args.seed!=1234 else snapshot_path
+    snapshot_path = snapshot_path + 'ACDCFancyPro3_epoch250'
+
+    if not os.path.exists(snapshot_path):
+        os.makedirs(snapshot_path)
+    
+    net = ResUnet(channel=3, img_size=args.img_size, num_class=args.num_classes).cuda()
+    
+    trainer = {'Synapse': trainer_synapse,'ACDC': trainer_acdc,}
+    trainer[dataset_name](args, net, snapshot_path)
---
+++ utils.py
@@ -0,0 +1,242 @@
+import numpy as np
+import torch
+from medpy import metric
+from scipy.ndimage import zoom
+import torch.nn as nn
+import SimpleITK as sitk
+
+
+class TI_Loss(torch.nn.Module):
+    def __init__(self, dim, connectivity, inclusion, exclusion, min_thick=1):
+        """
+        :param dim: 2 if 2D; 3 if 3D
+        :param connectivity: 4 or 8 for 2D; 6 or 26 for 3D
+        :param inclusion: list of [A,B] classes where A is completely surrounded by B.
+        :param exclusion: list of [A,C] classes where A and C exclude each other.
+        :param min_thick: Minimum thickness/separation between the two classes. Only used if connectivity is 8 for 2D or 26 for 3D
+        """
+        super(TI_Loss, self).__init__()
+
+        self.dim = dim
+        self.connectivity = connectivity
+        self.min_thick = min_thick
+        self.interaction_list = []
+        self.sum_dim_list = None
+        self.conv_op = None
+        self.apply_nonlin = lambda x: torch.nn.functional.softmax(x, 1)
+        self.ce_loss_func = torch.nn.CrossEntropyLoss(reduction='none')
+
+        if self.dim == 2 : 
+            self.sum_dim_list = [1,2,3]
+            self.conv_op = torch.nn.functional.conv2d
+        elif self.dim == 3 :
+            self.sum_dim_list = [1,2,3,4]
+            self.conv_op = torch.nn.functional.conv3d
+
+        self.set_kernel()
+
+        for inc in inclusion:
+            temp_pair = []
+            temp_pair.append(True) # type inclusion
+            temp_pair.append(inc[0])
+            temp_pair.append(inc[1])
+            self.interaction_list.append(temp_pair)
+
+        for exc in exclusion:
+            temp_pair = []
+            temp_pair.append(False) # type exclusion
+            temp_pair.append(exc[0])
+            temp_pair.append(exc[1])
+            self.interaction_list.append(temp_pair)
+
+
+    def set_kernel(self):
+        """
+        Sets the connectivity kernel based on user's sepcification of dim, connectivity, min_thick
+        """
+        k = 2 * self.min_thick + 1
+        if self.dim == 2:
+            if self.connectivity == 4:
+                np_kernel = np.array([[0,1,0],[1,1,1],[0,1,0]])
+            elif self.connectivity == 8:
+                np_kernel = np.ones((k, k))
+
+        elif self.dim == 3:
+            if self.connectivity == 6:
+                np_kernel = np.array([
+                                        [[0,0,0],[0,1,0],[0,0,0]],
+                                        [[0,1,0],[1,1,1],[0,1,0]],
+                                        [[0,0,0],[0,1,0],[0,0,0]]
+                                    ])
+            elif self.connectivity == 26:
+                np_kernel = np.ones((k, k, k))
+        
+        self.kernel = torch_kernel = torch.from_numpy(np.expand_dims(np.expand_dims(np_kernel,axis=0), axis=0))
+
+
+    def topological_interaction_module(self, P):
+        """
+        Given a discrete segmentation map and the intended topological interactions, this module computes the critical voxels map.
+        :param P: Discrete segmentation map
+        :return: Critical voxels map
+        """
+
+        for ind, interaction in enumerate(self.interaction_list):
+            interaction_type = interaction[0]
+            label_A = interaction[1]
+            label_C = interaction[2]
+
+            # Get Masks
+            mask_A = torch.where(P == label_A, 1.0, 0.0).double()
+            if interaction_type:
+                mask_C = torch.where(P == label_C, 1.0, 0.0).double()
+                mask_C = torch.logical_or(mask_C, mask_A).double()
+                mask_C = torch.logical_not(mask_C).double()
+            else:
+                mask_C = torch.where(P == label_C, 1.0, 0.0).double()
+            
+            # Get Neighbourhood Information
+            neighbourhood_C = self.conv_op(mask_C, self.kernel.double(), padding='same')
+            neighbourhood_C = torch.where(neighbourhood_C >= 1.0, 1.0, 0.0)
+            neighbourhood_A = self.conv_op(mask_A, self.kernel.double(), padding='same')
+            neighbourhood_A = torch.where(neighbourhood_A >= 1.0, 1.0, 0.0)
+
+            # Get the pixels which induce errors
+            violating_A = neighbourhood_C * mask_A
+            violating_C = neighbourhood_A * mask_C
+            violating = violating_A + violating_C
+            violating = torch.where(violating >= 1.0, 1.0, 0.0)
+
+            if ind == 0:
+                critical_voxels_map = violating
+            else:
+                critical_voxels_map = torch.logical_or(critical_voxels_map, violating).double()
+
+        return critical_voxels_map
+
+
+    def forward(self, x, y):
+        """
+        The forward function computes the TI loss value.
+        :param x: Likelihood map of shape: b, c, x, y(, z) with c = total number of classes
+        :param y: GT of shape: b, c, x, y(, z) with c=1. The GT should only contain values in [0,L) range where L is the total number of classes.
+        :return:  TI loss value
+        """
+
+        if x.device.type == "cuda":
+            self.kernel = self.kernel.cuda(x.device.index)
+        
+        if len(y.shape) == 3:
+            y = torch.unsqueeze(y, dim=1)
+
+        # Obtain discrete segmentation map
+        x_softmax = self.apply_nonlin(x)
+        P = torch.argmax(x_softmax, dim=1)
+        P = torch.unsqueeze(P.double(),dim=1)
+        del x_softmax
+
+        # Call the Topological Interaction Module
+        critical_voxels_map = self.topological_interaction_module(P)
+
+        # Compute the TI loss value
+        ce_tensor = torch.unsqueeze(self.ce_loss_func(x.double(),y[:,0].long()),dim=1)
+        ce_tensor[:,0] = ce_tensor[:,0] * torch.squeeze(critical_voxels_map, dim=1)
+        ce_loss_value = ce_tensor.sum(dim=self.sum_dim_list).mean()
+
+        return ce_loss_value
+
+
+class DiceLoss(nn.Module):
+    def __init__(self, n_classes):
+        super(DiceLoss, self).__init__()
+        self.n_classes = n_classes
+
+    def _one_hot_encoder(self, input_tensor):
+        tensor_list = []
+        for i in range(self.n_classes):
+            temp_prob = input_tensor == i  # * torch.ones_like(input_tensor)
+            tensor_list.append(temp_prob.unsqueeze(1))
+        output_tensor = torch.cat(tensor_list, dim=1)
+        return output_tensor.float()
+
+    def _dice_loss(self, score, target):
+        target = target.float()
+        smooth = 1e-5
+        intersect = torch.sum(score * target)
+        y_sum = torch.sum(target * target)
+        z_sum = torch.sum(score * score)
+        loss = (2 * intersect + smooth) / (z_sum + y_sum + smooth)
+        loss = 1 - loss
+        return loss
+
+    def forward(self, inputs, target, weight=None, softmax=False):
+        if softmax:
+            inputs = torch.softmax(inputs, dim=1)
+        target = self._one_hot_encoder(target)
+        if weight is None:
+            weight = [1] * self.n_classes
+        assert inputs.size() == target.size(), 'predict {} & target {} shape do not match'.format(inputs.size(), target.size())
+        class_wise_dice = []
+        loss = 0.0
+        for i in range(0, self.n_classes):
+            dice = self._dice_loss(inputs[:, i], target[:, i])
+            class_wise_dice.append(1.0 - dice.item())
+            loss += dice * weight[i]
+        return loss / self.n_classes
+
+
+def calculate_metric_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.dc(pred, gt)
+        hd95 = metric.binary.hd95(pred, gt)
+        return dice, hd95
+    elif pred.sum() > 0 and gt.sum()==0:
+        return 1, 0
+    else:
+        return 0, 0
+
+
+def test_single_volume(image, label, net, classes, patch_size=[256, 256], test_save_path=None, case=None, z_spacing=1):
+    image, label = image.squeeze(0).cpu().detach().numpy(), label.squeeze(0).cpu().detach().numpy()
+    if len(image.shape) == 3:
+        prediction = np.zeros_like(label)
+        for ind in range(image.shape[0]):
+            slice = image[ind, :, :]
+            x, y = slice.shape[0], slice.shape[1]
+            if x != patch_size[0] or y != patch_size[1]:
+                slice = zoom(slice, (patch_size[0] / x, patch_size[1] / y), order=3)  # previous using 0
+            input = torch.from_numpy(slice).unsqueeze(0).unsqueeze(0).float().cuda()
+            net.eval()
+            with torch.no_grad():
+                outputs = net(input)
+                out = torch.argmax(torch.softmax(outputs, dim=1), dim=1).squeeze(0)
+                out = out.cpu().detach().numpy()
+                if x != patch_size[0] or y != patch_size[1]:
+                    pred = zoom(out, (x / patch_size[0], y / patch_size[1]), order=0)
+                else:
+                    pred = out
+                prediction[ind] = pred
+    else:
+        input = torch.from_numpy(image).unsqueeze(
+            0).unsqueeze(0).float().cuda()
+        net.eval()
+        with torch.no_grad():
+            out = torch.argmax(torch.softmax(net(input), dim=1), dim=1).squeeze(0)
+            prediction = out.cpu().detach().numpy()
+    metric_list = []
+    for i in range(1, classes):
+        metric_list.append(calculate_metric_percase(prediction == i, label == i))
+
+    if test_save_path is not None:
+        img_itk = sitk.GetImageFromArray(image.astype(np.float32))
+        prd_itk = sitk.GetImageFromArray(prediction.astype(np.float32))
+        lab_itk = sitk.GetImageFromArray(label.astype(np.float32))
+        img_itk.SetSpacing((1, 1, z_spacing))
+        prd_itk.SetSpacing((1, 1, z_spacing))
+        lab_itk.SetSpacing((1, 1, z_spacing))
+        sitk.WriteImage(prd_itk, test_save_path + '/'+case + "_pred.nii.gz")
+        sitk.WriteImage(img_itk, test_save_path + '/'+ case + "_img.nii.gz")
+        sitk.WriteImage(lab_itk, test_save_path + '/'+ case + "_gt.nii.gz")
+    return metric_list
